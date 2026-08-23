8d3a4a2f4f3b3aa034313822675431029aab476a
lpow100 <lgn.kocher@gmail.com>
2026-08-22 22:09:47 -0400

    working

---
+++ Content/Items/Equipment/Summoner/ShinedStone.cs
@@ -0,0 +1,27 @@
+using Anabasis.Common.DamageClasses;
+using Anabasis.Common.Players;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Anabasis.Content.Items.Equipment.Summoner
+{
+    public class ShinedStone : ModItem
+    {
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+
+            Item.accessory = true;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.buyPrice(silver: 45);
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.GetDamage<SummonDamageClass>().Flat += 1;
+        }
+    }
+}
